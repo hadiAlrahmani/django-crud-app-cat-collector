dca19443dd6fbd4ffdaae327bb98e050618fbe8e
Hiddensti <alrahmanihadi@gmail.com>
2025-03-17 14:34:33 +0300

    part 2 works

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,46 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Cat
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html', {'title': 'About'})
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url= '/cats/'
+    
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+class CatDelete(DeleteView):
+        model = Cat
+        success_url= '/cats/'
